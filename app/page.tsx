93e1f06167fd8919e98103ca76e64c5657cd7fa8
Daniele Buratti <danibura2007@gmail.com>
2026-07-23 19:12:34 +0200

    Changed expenses tracker cover

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const projects: ProjectType[] = [
     title: "Expense tracker",
     description: "A web app to monitor your expenses and earnings",
     url: "https://budgeting.danibura.me",
-    cover: "budgeting.webp",
+    cover: "budgeting2.webp",
     languages: ["tailwind", "typescript", "react", "nextjs", "postgresql"],
   },
   {
